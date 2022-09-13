7a28c383dc6865cdeed6dc2526823a996b7706ba
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-09-13 12:38:54 +0300

    Upload basic site framework

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route("/")
+def home():
+    # intro = "Hello, I'm William and this is my portfolio website."
+    # print(len(intro))
+    return render_template("index.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
