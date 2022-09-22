ad185672f4d451127102c5fbc8627fb1dab1f434
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-09-22 19:43:48 +0300

    Add styling and js code for
    displaying navbar current page indication.

--- static/min.js
+++ static/min.js
@@ -1,1 +1,9 @@
 console.log("Rock and roll!")
+
+document.querySelectorAll('.nav-link').forEach
+(link => {
+    if(link.href === window.location.href) {
+        link.setAttribute('aria-current', 'page')
+        link.setAttribute('class', 'nav-link active')
+    }
+})
