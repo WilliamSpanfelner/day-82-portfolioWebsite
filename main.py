353d00a973a9654388f4fa8b46fc977e577f1282
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-09-30 20:08:05 +0300

    Create year global to be passed
    to different routes;

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ from datetime import date
 
 app = Flask(__name__)
 Bootstrap(app)
+year = date.today().year
 
 
 @app.route("/")
 def about():
     # intro = "Hello, I'm William and this is my portfolio website."
     # print(len(intro))
-    year = date.today().year
     return render_template("index.html", yr=year)
 
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template("portfolio.html")
+    return render_template("portfolio.html", yr=year)
 
 
 @app.route("/contact")
 def contact():
     # TODO: Write the email code here.
-    return render_template("contact.html")
+    return render_template("contact.html", yr=year)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4000)
+    app.run(host="0.0.0.0", port=5000)
