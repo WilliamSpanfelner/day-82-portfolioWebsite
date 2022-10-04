f1608c3e3d14400392926324475995788303e9e4
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-10-04 14:52:03 +0300

    Add projects and links data and pass with
    routing to html docs.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import random
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from datetime import date
+from data import projects, links
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -11,18 +13,19 @@ year = date.today().year
 def about():
     # intro = "Hello, I'm William and this is my portfolio website."
     # print(len(intro))
-    return render_template("index.html", yr=year)
+    return render_template("index.html", links=links, yr=year)
 
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template("portfolio.html", yr=year)
+    # random.shuffle(projects)
+    return render_template("portfolio.html", all_projects=projects, links=links, yr=year)
 
 
 @app.route("/contact")
 def contact():
     # TODO: Write the email code here.
-    return render_template("contact.html", yr=year)
+    return render_template("contact.html", links=links, yr=year)
 
 
 if __name__ == "__main__":
